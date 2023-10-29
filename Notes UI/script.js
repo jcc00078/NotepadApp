7e3eb97bec7e54d83b574e2f864fba2fca27d5c0
Juan Carmona Cortes <juancarmona277@gmail.com>
2023-10-29 18:20:30 +0100

    Cliente finalizado

--- Notes UI/script.js
+++ Notes UI/script.js
@@ -2,10 +2,30 @@ const botonGuardar = document.querySelector("#bGuardar");
 const inputDescripcion = document.querySelector("#descripcion");
 const inputTitulo = document.querySelector("#titulo");
 const notesContainer = document.querySelector("#notes-container");
+const botonBorrar = document.querySelector("#bBorrar");
+
+function rellenarFormulario(id) {
+  getNoteById(id);
+}
+function displayNoteInForm(note) {
+  inputTitulo.value = note.title;
+  inputDescripcion.value = note.description;
+  botonBorrar.classList.remove("hidden");
+  botonBorrar.setAttribute("data-id", note.id);
+  botonGuardar.setAttribute("data-id", note.id);
+}
+function getNoteById(id) {
+  fetch(`https://localhost:7269/api/Notes/${id}`, {
+    method: "GET",
+  })
+    .then((data) => data.json())
+    .then((response) => displayNoteInForm(response));
+}
 
 function clearData() {
   inputTitulo.value = "";
   inputDescripcion.value = "";
+  botonBorrar.classList.add("hidden");
 }
 function createNote(title, description) {
   const body = {
@@ -24,6 +44,7 @@ function createNote(title, description) {
     .then((response) => {
       clearData();
       getAllNotes();
+      botonGuardar.removeAttribute("data-id");
     });
 }
 
@@ -31,7 +52,7 @@ function displayNotes(notes) {
   var allNotes = "";
   notes.forEach((note) => {
     const noteElement = `
-    <div class="note">
+    <div class="note" data-id=${note.id}>
           <h3>${note.title}</h3>
           <p>
             ${note.description}
@@ -41,6 +62,12 @@ function displayNotes(notes) {
     allNotes += noteElement;
   });
   notesContainer.innerHTML = allNotes;
+
+  document.querySelectorAll(".note").forEach((note) => {
+    note.addEventListener("click", function () {
+      rellenarFormulario(note.dataset.id);
+    });
+  });
 }
 
 function getAllNotes() {
@@ -53,6 +80,51 @@ function getAllNotes() {
 
 getAllNotes();
 
+function updateNote(id, title, description) {
+  const body = {
+    title: title,
+    description: description,
+    isVisible: true,
+  };
+  fetch(`https://localhost:7269/api/Notes/${id}`, {
+    method: "PUT",
+    body: JSON.stringify(body),
+    headers: {
+      "content-type": "application/json",
+    },
+  })
+    .then((data) => data.json())
+    .then((response) => {
+      clearData();
+      getAllNotes();
+      botonGuardar.removeAttribute("data-id");
+    });
+}
+
 botonGuardar.addEventListener("click", function () {
-  createNote(inputTitulo.value, inputDescripcion.value);
+  const id = botonGuardar.dataset.id;
+  //Si ya existe el id, actualizo la nota (no creo una nueva)
+  if (id) {
+    updateNote(id, inputTitulo.value, inputDescripcion.value);
+  } else {
+    createNote(inputTitulo.value, inputDescripcion.value);
+  }
+});
+
+function deleteNote(id) {
+  fetch(`https://localhost:7269/api/Notes/${id}`, {
+    method: "DELETE",
+    headers: {
+      "content-type": "application/json",
+    },
+  }).then((response) => {
+    clearData();
+    getAllNotes();
+    botonBorrar.removeAttribute("data-id");
+  });
+}
+
+botonBorrar.addEventListener("click", function () {
+  const id = botonBorrar.dataset.id;
+  deleteNote(id);
 });
